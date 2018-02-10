e92d75476ddfa527f688fc70db02ec27b2e66646
Rob Maynard <robmaynardjr@gmail.com>
2018-02-10 01:20:22 -0500

    UPDATED Get-vmSnapshots.ps1: Added bsic functionality.

--- BasicFunctions/Get-vmSnapshots.ps1
+++ BasicFunctions/Get-vmSnapshots.ps1
@@ -17,30 +17,31 @@
     Version 0.1
     Created by Rob Maynard Jr.
     February 9, 2018
+
+    Requires: Powershell 5.0.0+
+              VMware PowerCLI 6.5.0+  
 #>
 function Get-vmSnapShots
 {
-    [CmdletBinding()]
-    [Alias("gvmss")]    
+    [CmdletBinding(DefaultParameterSetName='0')]
+    [Alias("gvmss")]
+
     Param
     (
         # PARAMETER SET 0 (DEFAULT)
         # Enter the vCenter Server(s) to search for VM snapshots.
-        [Parameter(Mandatory=$true,
-                    ValueFromPipeline=$true,
-                   ValueFromPipelineByPropertyName=$true,
-                   Position=0,ParameterSetName='0')]
-        [ValidateNotNullorEmpty()]
+        [Parameter(Mandatory=$true,                   
+                   ValueFromPipelineByPropertyName=$true,                   
+                   Position=0,ParameterSetName='0')]        
 
-        [VMware.VimAutomation.Types.VIServer[]]
+        [string[]]
         $vCenter,
 
         #PARAMETER SET 1 (OUTPUT)
         # If this flag is set, output will be saved to a report or file.
-        [Parameter(Mandatory=$false,
-                    ValueFromPipeline=$true,
+        [Parameter(Mandatory=$false,                    
                    ValueFromPipelineByPropertyName=$true,
-                   Position=0,ParameterSetName='1')]
+                   Position=1,ParameterSetName='1')]
                    
         [switch]
         $OutFile,
@@ -49,32 +50,73 @@ function Get-vmSnapShots
         [Parameter(Mandatory=$true,
                     ValueFromPipeline=$true,
                    ValueFromPipelineByPropertyName=$true,
-                   Position=0,ParameterSetName='1')]
+                   Position=2,ParameterSetName='1')]
         [ValidateNotNullorEmpty()]
         [ValidateSet("csv", "html", "txt")]
 
         [string]
         $OuputType
+
+        <#
+        #PARAMETER SET 2 (Credentials)
+        # Set Username and password to use
+        [Parameter(Mandatory=$false,
+                    ValueFromPipeline=$true,
+                   ValueFromPipelineByPropertyName=$true,
+                   Position=4,ParameterSetName='2')]
+        #[ValidateNotNullOrEmpty()]
         
+        [string]
+        $User,
+
+        [string]
+        $Pass
+        #>       
     )
-    #Validate, Connect, Pull Data From Servers
+
+    #Validate PowerCLI and PS versions, Connect to vCenter Servers
     Begin
     {
-        try {
-            Connect-VIServer -Server $vCenter -Credential (Get-Credential) -SaveCredentials 
-        }
-        catch {
-            throw $Error[0]
+        Get-Module -ListAvailable VM* | Import-Module
+        $PcliVer = Get-Module VMware.VimAutomation.Core | Select-Object 
+        if ($PcliVer.Version.Major -ge 6 -and $PcliVer.Version.Minor -ge 5 -and $PSVersionTable.PSVersion.Major -ge 5) {
+
+            try {
+                Connect-VIServer -Server $vCenter       
+            }                        
+            catch {
+                throw $Error[0]
+                break
+            }
+        }        
+        else {
+            throw 'This cmdlet requires PowerCLI 6.5.0+ and Powershell 5.0+'
             break
-        }
+        }               
     }
-    #Process Data
+
+    #Pull VM Snapshot Data From Servers, Process Data, Report Information
     Process
     {
+        $vcSnapshotArray = @()
+
+        foreach ($v in $vCenter) {
+            $snapShots = Get-VM -Server $v | Get-Snapshot
+            
+            foreach ($s in $snapShots) {
+                $snapObj = New-Object psobject
+                $snapObj | Add-Member -MemberType NoteProperty -Name VM -Value $s.VM
+                $snapObj | Add-Member -MemberType NoteProperty -Name Name -Value $s.Name
+                $snapObj | Add-Member -MemberType NoteProperty -Name Created -Value $s.Created
+                $snapObj | Add-Member -MemberType NoteProperty -Name SizeGB -Value ($s.SizeGB)
+                $vcSnapshotArray += $snapObj
+            }
+        }        
     }
-    #Output and Close
+
+    #Output, Display Reports and Close Connections
     End
     {
+        $vcSnapshotArray
     }
 }
-
